c63565716cb5eb998f0a2af895598c62282b7115
fred@macbook <frederic.zhang@anu.edu.au>
2020-10-13 22:03:15 +1100

    Add support for DistributedDataParallel & Add more arguments

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import os
 import torch
 import argparse
 import torchvision
-from torch.utils.data import DataLoader
+import torch.distributed as dist
+import torch.multiprocessing as mp
+from torch.utils.data import DataLoader, DistributedSampler
 
 import pocket
 from pocket.data import HICODet
@@ -10,7 +12,14 @@ from pocket.data import HICODet
 from models import InteractGraphNet
 from utils import custom_collate, CustomisedEngine, CustomisedDataset
 
-def train(args):
+def main(rank, args):
+
+    dist.init_process_group(
+        backend="nccl",
+        init_method="env://",
+        world_size=args.world_size,
+        rank=rank
+    )
 
     trainset = HICODet(
         root=os.path.join(args.data_root,
@@ -30,9 +39,12 @@ def train(args):
         target_transform=pocket.ops.ToTensor(input_format='dict')
     )
 
+    # Fix random seed for model synchronisation
+    torch.manual_seed(args.random_seed)
+
     net = InteractGraphNet(
         trainset.object_to_verb, 49,
-        num_iterations=1
+        num_iterations=args.num_iter
     )
     if os.path.exists(args.model_path):
         print("Loading model from ", args.model_path)
@@ -40,28 +52,40 @@ def train(args):
 
     engine = CustomisedEngine(
         net,
-        DataLoader(CustomisedDataset(
-            trainset, os.path.join(args.data_root,
+        DataLoader(
+            dataset=CustomisedDataset(trainset, 
+                os.path.join(args.data_root,
                 "fasterrcnn_resnet50_fpn_detections/train2015")
-        ), collate_fn=custom_collate, batch_size=8,
-        shuffle=True, num_workers=4, pin_memory=True),
-        DataLoader(CustomisedDataset(
-            testset, os.path.join(args.data_root,
+            ), collate_fn=custom_collate, batch_size=args.barch_size,
+            num_workers=args.num_workers, pin_memory=True,
+            sampler=DistributedSampler(
+                trainset, 
+                num_replicas=args.world_size, 
+                rank=rank)
+        ),
+        DataLoader(
+            dataset=CustomisedDataset(testset,
+                os.path.join(args.data_root,
                 "fasterrcnn_resnet50_fpn_detections/test2015")
-        ), collate_fn=custom_collate, batch_size=8,
-        num_workers=4, pin_memory=True),
+            ), collate_fn=custom_collate, batch_size=args.batch_size,
+            num_workers=args.num_workers, pin_memory=True,
+            sampler=DistributedSampler(
+                testset,
+                num_replicas=args.world_size,
+                rank=rank)
+        ),
         num_classes=trainset.num_action_cls,
         optim_params={
-            'lr': 0.001,
-            'momentum': 0.9,
-            'weight_decay': 1e-4
+            'lr': args.learning_rate,
+            'momentum': args.momentum,
+            'weight_decay': args.weight_decay
         },
         lr_scheduler=True,
         lr_sched_params={
-            'milestones': [6, 14],
-            'gamma': 0.1
+            'milestones': args.milestone,
+            'gamma': args.lr_decay
         },
-        print_interval=2000,
+        print_interval=args.print_interval,
         cache_dir=args.cache_dir
     )
 
@@ -70,30 +94,30 @@ def train(args):
 if __name__ == '__main__':
     
     parser = argparse.ArgumentParser(description="Train an interaction head")
-    parser.add_argument('--data-root',
-                        required=True,
-                        type=str)
-    parser.add_argument('--num-epochs',
-                        type=int,
-                        default=25)
-    parser.add_argument('--num-box-pairs-per-image',
-                        type=int,
-                        default=512)
-    parser.add_argument('--positive-ratio',
-                        type=float,
-                        default=0.25)
-    parser.add_argument('--box-score-thresh',
-                        type=float,
-                        default=0.2)
-    parser.add_argument('--model-path',
-                        default='',
-                        type=str)
-    parser.add_argument('--cache-dir',
-                        type=str,
-                        default='./checkpoints')
-    parser.add_argument('--masked-pool',
-                        action='store_true',
-                        default=False)
+    parser.add_argument('--data-root', required=True, type=str)
+    parser.add_argument('--world-size', required=True, type=int,
+                        help="Number of subprocesses/GPUs to use")
+    parser.add_argument('--num-iter', default=1, type=int,
+                        help="Number of iterations to run message passing")
+    parser.add_argument('--num-epochs', default=20, type=int)
+    parser.add_argument('--random-seed', default=1, type=int)
+    parser.add_argument('--learning-rate', default=0.001, type=float)
+    parser.add_argument('--momentum', default=0.9, type=float)
+    parser.add_argument('--weight-decay', default=1e-4, type=float)
+    parser.add_argument('--batch-size', default=2, type=int,
+                        help="Batch size for each subprocess")
+    parser.add_argument('--lr-decay', default=0.1, type=float,
+                        help="The multiplier by which the learning rate is reduced")
+    parser.add_argument('--milestones', nargs='+', default=[10, 15],
+                        help="The epoch number when learning rate is reduced")
+    parser.add_argument('--num-workers', default=2, type=int)
+    parser.add_argument('--print-interval', default=100, type=int)
+    parser.add_argument('--model-path', default='', type=str)
+    parser.add_argument('--cache-dir', type=str, default='./checkpoints')
+
     args = parser.parse_args()
 
-    train(args)
+    os.environ["MASTER_ADDR"] = "localhost"
+    os.environ["MASTER_PORT"] = "8888"
+
+    mp.spawn(main, nprocs=args.world_size, args=(args,))
