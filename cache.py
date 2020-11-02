cd60666ee4b4448627bd41a63b81b4bc3978619b
fred@macbook <frederic.zhang@anu.edu.au>
2020-11-02 21:08:06 +1100

    Add dataset partition as an argument & Update network output format

--- cache.py
+++ cache.py
@@ -1,3 +1,12 @@
+"""
+Run inference and cache detected box pairs and predictions
+
+Fred Zhang <frederic.zhang@anu.edu.au>
+
+The Australian National University
+Australian Centre for Robotic Vision
+"""
+
 import os
 import torch
 import pickle
@@ -28,30 +37,28 @@ def inference(net, dataloader, batch_size):
         for j, result in enumerate(output):
             filename = dataset.filename(i*batch_size+j).split('.')[0]
 
-            # Repeat boxes for each predicted class
-            repeat_num = [len(l) for l in result['labels']]
-            boxes_h = torch.cat([
-                b_h.repeat(num, 1) for b_h, num in zip(result['boxes_h'], repeat_num)
-            ])
-            boxes_o = torch.cat([
-                b_o.repeat(num, 1) for b_o, num in zip(result['boxes_o'], repeat_num)
-            ])
-            interaction_class = torch.Tensor([
+            box_idx = result['index']
+            boxes_h = result['boxes_h'][box_idx]
+            boxes_o = result['boxes_o'][box_idx]
+            objects = result['object'][box_idx]
+
+            scores = result['scores']
+            verbs = result['prediction']
+            interactions = torch.tensor([
                 dataset.object_n_verb_to_interaction[o][v]
-                for o, l in zip(result['object'], result['labels']) for v in l
+                for o, v in zip(objects, verbs)
             ])
-            scores = torch.cat(result['scores'])
 
             # Group box pairs with the same predicted class
-            permutation = interaction_class.argsort()
+            permutation = interactions.argsort()
             boxes_h = boxes_h[permutation]
             boxes_o = boxes_o[permutation]
-            interaction_class = interaction_class[permutation]
+            interactions = interactions[permutation]
             scores = scores[permutation]
 
             # Record start and end index of box pairs for each class
             idx = torch.zeros(600, 2)
-            unique_class, counts = interaction_class.unique(return_counts=True)
+            unique_class, counts = interactions.unique(return_counts=True)
             n = 0
             for cls_id, cls_num in zip(unique_class, counts):
                 idx[cls_id.long(), 0] = n
@@ -72,25 +79,25 @@ def main(args):
     torch.cuda.set_device(0)
     torch.backends.cudnn.benchmark = False
 
-    testset = HICODet(
+    dataset = HICODet(
         root=os.path.join(args.data_root,
-            "hico_20160224_det/images/test2015"),
+            "hico_20160224_det/images/{}".format(args.partition)),
         annoFile=os.path.join(args.data_root,
-            "instances_test2015.json"),
+            "instances_{}.json".format(args.partition)),
         transform=torchvision.transforms.ToTensor(),
         target_transform=pocket.ops.ToTensor(input_format='dict')
     )    
-    test_loader = DataLoader(
-        dataset=CustomisedDataset(testset,
+    dataloader = DataLoader(
+        dataset=CustomisedDataset(dataset,
             os.path.join(args.data_root,
-            "fasterrcnn_resnet50_fpn_detections/test2015"),
+            "fasterrcnn_resnet50_fpn_detections/{}".format(args.parition)),
             human_idx=49
         ), collate_fn=custom_collate, batch_size=args.batch_size,
         num_workers=args.num_workers, pin_memory=True
     )
 
     net = InteractGraphNet(
-        testset.object_to_verb, 49,
+        dataset.object_to_verb, 49,
         num_iterations=args.num_iter
     )
     if os.path.exists(args.model_path):
@@ -100,11 +107,12 @@ def main(args):
 
     net.cuda()
     
-    inference(net, test_loader, args.batch_size)
+    inference(net, dataloader, args.batch_size)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Train an interaction head")
     parser.add_argument('--data-root', required=True, type=str)
+    parser.add_argument('--partition', default='test2015', type=str)
     parser.add_argument('--num-iter', default=3, type=int,
                         help="Number of iterations to run message passing")
     parser.add_argument('--batch-size', default=2, type=int,
