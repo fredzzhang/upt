95ba6a5673ce027c758d7e83e5cd4643371ea60f
fred@woodside <frederic.zhang@anu.edu.au>
2020-12-21 21:45:18 +1100

    (#6) Add scripts to download fine-tuned detections

---
+++ download/test2015_finetuned_vcl.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+FILE=test2015_finetuned_vcl.tar.gz
+ID=1eMj8DON8NkutD6kWT_U-xiP4B5jsKXF6
+
+if [ -f $FILE ]; then
+  echo "$FILE already exists."
+  exit 0
+fi
+
+echo "Connecting..."
+
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&id=$ID" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1/p')&id=$ID" -O $FILE && rm -rf /tmp/cookies.txt
+
+echo "Extracting..."
+
+tar zxf $FILE
+
+echo "Done."
