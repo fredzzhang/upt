244b3265d07203e06b54d877066ab599c878e513
fred@macbook <frederic.zhang@anu.edu.au>
2020-12-25 15:08:50 +1100

    (#6) Remove redundant code

--- download/download_finetuned_detections.sh
+++ download/download_finetuned_detections.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-DATADIR=../hicodet/detections
-
 bash test2015_finetuned_drg.sh
 bash test2015_finetuned_vcl.sh
 bash test2015_finetuned_ours.sh
-
-mv test2015_finetuned_drg $DATADIR/
-mv test2015_finetuned_vcl $DATADIR/
-mv test2015_finetuned_ours $DATADIR/
