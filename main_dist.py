b6166586da4cdeb075258c5d4bad6d2913ff4047
fred@macbook <frederic.zhang@anu.edu.au>
2020-12-25 22:49:15 +1100

    Restructure code

--- main_dist.py
+++ main_dist.py
@@ -19,7 +19,7 @@ import pocket
 from pocket.data import HICODet
 
 from models import SpatioAttentiveGraph
-from utils import custom_collate, CustomisedEngine, CustomisedDataset
+from utils import custom_collate, CustomisedDataset, CustomisedDLE
 
 def main(rank, args):
 
@@ -46,6 +46,37 @@ def main(rank, args):
         target_transform=pocket.ops.ToTensor(input_format='dict')
     )
 
+    train_loader = DataLoader(
+        dataset=CustomisedDataset(trainset, 
+            os.path.join(args.data_root,
+            "detections/train2015"),
+            human_idx=49, 
+            box_score_thresh_h=args.human_thresh,
+            box_score_thresh_o=args.object_thresh,
+            flip=True
+        ), collate_fn=custom_collate, batch_size=args.batch_size,
+        num_workers=args.num_workers, pin_memory=True,
+        sampler=DistributedSampler(
+            trainset, 
+            num_replicas=args.world_size, 
+            rank=rank)
+    )
+
+    val_loader = DataLoader(
+        dataset=CustomisedDataset(testset, 
+            os.path.join(args.data_root,
+            "detections/test2015"),
+            human_idx=49,
+            box_score_thresh_h=args.human_thresh,
+            box_score_thresh_o=args.object_thresh
+        ), collate_fn=custom_collate, batch_size=args.batch_size,
+        num_workers=args.num_workers, pin_memory=True,
+        sampler=DistributedSampler(
+            testset, 
+            num_replicas=args.world_size, 
+            rank=rank)
+    )
+
     # Fix random seed for model synchronisation
     torch.manual_seed(args.random_seed)
 
@@ -59,39 +90,13 @@ def main(rank, args):
         p.requires_grad = False
 
     if os.path.exists(args.model_path):
-        print("Loading model from ", args.model_path)
         net.load_state_dict(torch.load(args.model_path)['model_state_dict'])
+        print("Model loaded from ", args.model_path)
 
-    engine = CustomisedEngine(
+    engine = CustomisedDLE(
         net,
-        DataLoader(
-            dataset=CustomisedDataset(trainset, 
-                os.path.join(args.data_root,
-                "detections/train2015"),
-                human_idx=49, 
-                box_score_thresh_h=args.human_thresh,
-                box_score_thresh_o=args.object_thresh
-            ), collate_fn=custom_collate, batch_size=args.batch_size,
-            num_workers=args.num_workers, pin_memory=True,
-            sampler=DistributedSampler(
-                trainset, 
-                num_replicas=args.world_size, 
-                rank=rank)
-        ),
-        DataLoader(
-            dataset=CustomisedDataset(testset, 
-                os.path.join(args.data_root,
-                "detections/test2015"),
-                human_idx=49,
-                box_score_thresh_h=args.human_thresh,
-                box_score_thresh_o=args.object_thresh
-            ), collate_fn=custom_collate, batch_size=args.batch_size,
-            num_workers=args.num_workers, pin_memory=True,
-            sampler=DistributedSampler(
-                testset, 
-                num_replicas=args.world_size, 
-                rank=rank)
-        ),
+        train_loader,
+        val_loader,
         optim_params={
             'lr': args.learning_rate,
             'momentum': args.momentum,
@@ -110,27 +115,27 @@ def main(rank, args):
 
 if __name__ == '__main__':
     
-    parser = argparse.ArgumentParser(description="Train an interaction head")
-    parser.add_argument('--data-root', required=True, type=str)
+    parser = argparse.ArgumentParser()
     parser.add_argument('--world-size', required=True, type=int,
                         help="Number of subprocesses/GPUs to use")
-    parser.add_argument('--num-iter', default=1, type=int,
+    parser.add_argument('--data-root', default='hicodet', type=str)
+    parser.add_argument('--num-iter', default=2, type=int,
                         help="Number of iterations to run message passing")
-    parser.add_argument('--num-epochs', default=20, type=int)
+    parser.add_argument('--num-epochs', default=15, type=int)
     parser.add_argument('--random-seed', default=1, type=int)
     parser.add_argument('--learning-rate', default=0.001, type=float)
     parser.add_argument('--momentum', default=0.9, type=float)
     parser.add_argument('--weight-decay', default=1e-4, type=float)
-    parser.add_argument('--batch-size', default=2, type=int,
+    parser.add_argument('--batch-size', default=4, type=int,
                         help="Batch size for each subprocess")
     parser.add_argument('--lr-decay', default=0.1, type=float,
                         help="The multiplier by which the learning rate is reduced")
-    parser.add_argument('--human-thresh', default=0.5, type=float)
-    parser.add_argument('--object-thresh', default=0.5, type=float)
-    parser.add_argument('--milestones', nargs='+', default=[10, 15],
+    parser.add_argument('--human-thresh', default=0.2, type=float)
+    parser.add_argument('--object-thresh', default=0.2, type=float)
+    parser.add_argument('--milestones', nargs='+', default=[10,],
                         help="The epoch number when learning rate is reduced")
-    parser.add_argument('--num-workers', default=2, type=int)
-    parser.add_argument('--print-interval', default=100, type=int)
+    parser.add_argument('--num-workers', default=4, type=int)
+    parser.add_argument('--print-interval', default=2000, type=int)
     parser.add_argument('--model-path', default='', type=str)
     parser.add_argument('--cache-dir', type=str, default='./checkpoints')
 
