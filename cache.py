735ea10651daa2ee148acbad726f1aa073748a25
fredzzhang <frederic.zhang@anu.edu.au>
2021-01-15 16:38:25 +1100

    (#19) Relocate class to the top-level for picklability

--- cache.py
+++ cache.py
@@ -90,20 +90,22 @@ def inference_hicodet(net, dataloader, coco2hico, cache_dir):
             dict(all_boxes=all_results[interaction_idx])
         )
 
+class CacheTemplate(defaultdict):
+    """A template for VCOCO cached results """
+    def __init__(self, **kwargs):
+        super().__init__()
+        for k, v in kwargs.items():
+            self[k] = v
+    def __missing__(self, k):
+        seg = k.split('_')
+        # Assign zero score to missing actions
+        if seg[-1] == 'agent':
+            return 0.
+        # Assign zero score and a tiny box to missing <action,role> pairs
+        else:
+            return [0., 0., .1, .1, 0.]
+
 def inference_vcoco(net, dataloader, cache_dir):
-    class CacheTemplate(defaultdict):
-        def __init__(self, **kwargs):
-            super().__init__()
-            for k, v in kwargs.items():
-                self[k] = v
-        def __missing__(self, k):
-            seg = k.split('_')
-            # Assign zero score to missing actions
-            if seg[-1] == 'agent':
-                return 0.
-            # Assign zero score and a tiny box to missing <action,role> pairs
-            else:
-                return [0., 0., .1, .1, 0.]
     dataset = dataloader.dataset.dataset
     net.eval()
     all_results = []
