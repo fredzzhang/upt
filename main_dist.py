324637451465a6d700daef422ba6cddd6899fda8
fredzzhang <frederic.zhang@anu.edu.au>
2021-03-10 12:58:37 +1100

    Fine-tune the backbone with a lower learning rate
    Signed-off-by: fredzzhang <frederic.zhang@anu.edu.au>

--- main_dist.py
+++ main_dist.py
@@ -83,8 +83,8 @@ def main(rank, args):
         distributed=True
     )
     # Fix backbone parameters
-    for p in net.backbone.parameters():
-        p.requires_grad = False
+    # for p in net.backbone.parameters():
+    #     p.requires_grad = False
 
     if os.path.exists(args.checkpoint_path):
         print("=> Rank {}: continue from saved checkpoint".format(
@@ -109,18 +109,29 @@ def main(rank, args):
         print_interval=args.print_interval,
         cache_dir=args.cache_dir
     )
-    net_params = [
-        p for p in engine.fetch_state_key('net').parameters()
-        if p.requires_grad
-    ]
-    optim = torch.optim.AdamW(
-        net_params,
-        lr=args.learning_rate,
+    param_group_1 = []
+    param_group_2 = []
+    for k, v in engine.fetch_state_key('net').named_parameters():
+        if v.requires_grad:
+            if k.startswith('backbone'):
+                param_group_1.append(v)
+            elif k.startswith('interaction_head'):
+                param_group_2.append(v)
+            else:
+                raise KeyError("Unknown parameter name {k}")
+
+    optim = torch.optim.AdamW([
+        {'params': param_group_1, 'lr': args.learning_rate * args.lr_decay},
+        {'params': param_group_2}
+        ], lr=args.learning_rate,
         weight_decay=args.weight_decay
     )
-    lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(
-        optim, milestones=args.milestones, gamma=args.lr_decay
+    lambda1 = lambda epoch: 1.
+    lambda2 = lambda epoch: 1. if epoch < args.milestones[0] else args.lr_decay
+    lr_scheduler = torch.optim.lr_scheduler.LambdaLR(
+        optim, lr_lambda=[lambda1, lambda2]
     )
+
     engine.update_state_key(optimizer=optim, lr_scheduler=lr_scheduler)
     engine.update_state_key(epoch=epoch, iteration=iteration)
 
