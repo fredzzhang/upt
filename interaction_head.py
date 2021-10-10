ea92439994b1503d93c81f83e4b319f20182c513
fred@pegasus <frederic.zhang@anu.edu.au>
2021-10-10 21:16:25 +1100

    Rearrange order of classes
    Signed-off-by: fred@pegasus <frederic.zhang@anu.edu.au>

--- interaction_head.py
+++ interaction_head.py
@@ -22,6 +22,191 @@ import pocket
 
 from ops import compute_spatial_encodings, binary_focal_loss
 
+
+
+class MultiBranchFusion(Module):
+    """
+    Multi-branch fusion module
+
+    Parameters:
+    -----------
+    appearance_size: int
+        Size of the appearance features
+    spatial_size: int
+        Size of the spatial features
+    representation_size: int
+        Size of the intermediate representations
+    cardinality: int
+        The number of homogeneous branches
+    """
+    def __init__(self,
+        appearance_size: int, spatial_size: int,
+        representation_size: int, cardinality: int
+    ) -> None:
+        super().__init__()
+        self.cardinality = cardinality
+
+        sub_repr_size = int(representation_size / cardinality)
+        assert sub_repr_size * cardinality == representation_size, \
+            "The given representation size should be divisible by cardinality"
+
+        self.fc_1 = nn.ModuleList([
+            nn.Linear(appearance_size, sub_repr_size)
+            for _ in range(cardinality)
+        ])
+        self.fc_2 = nn.ModuleList([
+            nn.Linear(spatial_size, sub_repr_size)
+            for _ in range(cardinality)
+        ])
+        self.fc_3 = nn.ModuleList([
+            nn.Linear(sub_repr_size, representation_size)
+            for _ in range(cardinality)
+        ])
+    def forward(self, appearance: Tensor, spatial: Tensor) -> Tensor:
+        return F.relu(torch.stack([
+            fc_3(F.relu(fc_1(appearance) * fc_2(spatial)))
+            for fc_1, fc_2, fc_3
+            in zip(self.fc_1, self.fc_2, self.fc_3)
+        ]).sum(dim=0))
+
+class MatchingLayer(Module):
+    def __init__(self,
+        hidden_size: int = 1024,
+        num_heads: int = 8,
+        return_weights: bool = False,
+    ):
+        super().__init__()
+        if hidden_size % num_heads != 0:
+            raise ValueError(
+                f"The given hidden size {hidden_size} should be divisible by "
+                f"the number of attention heads {num_heads}."
+            )
+        self.sub_hidden_size = int(hidden_size / num_heads)
+        self.hidden_size = hidden_size
+        self.num_heads = num_heads
+        self.return_weights = return_weights
+
+        self.unary = nn.Linear(hidden_size, hidden_size)
+        self.pairwise = nn.Linear(hidden_size, hidden_size)
+        self.attn = nn.ModuleList([nn.Linear(3 * self.sub_hidden_size, 1) for _ in range(num_heads)])
+        self.message = nn.ModuleList([nn.Linear(self.sub_hidden_size, self.sub_hidden_size) for _ in range(num_heads)])
+        self.aggregate = nn.Linear(hidden_size, hidden_size)
+        self.norm = nn.LayerNorm(hidden_size)
+
+    def reshape(self, x: Tensor) -> Tensor:
+        new_x_shape = x.size()[:-1] + (
+            self.num_heads,
+            self.sub_hidden_size
+        )
+        x = x.view(*new_x_shape)
+        if len(new_x_shape) == 3:
+            return x.permute(1, 0, 2)
+        elif len(new_x_shape) == 4:
+            return x.permute(2, 0, 1, 3)
+        else:
+            raise ValueError("Incorrect tensor shape")
+
+    def forward(self, x: Tensor, y: Tensor):
+        device = x.device
+        n = len(x)
+
+        u = F.relu(self.unary(x))
+        p = F.relu(self.pairwise(y))
+
+        # Unary features (H, N, L)
+        u_r = self.reshape(u)
+        # Pairwise features (H, N, N, L)
+        p_r = self.reshape(p)
+
+        i, j = torch.meshgrid(
+            torch.arange(n, device=device),
+            torch.arange(n, device=device)
+        )
+
+        # Features used to compute attention (H, N, N, 3L)
+        attn_features = torch.cat([
+            u_r[:, i], u_r[:, j], p_r
+        ], dim=-1)
+        # Attention weights (H,) (N, N, 1)
+        weights = [
+            F.softmax(l(f), dim=0) for f, l
+            in zip(attn_features, self.attn)
+        ]
+        # Repeated unary feaures along the third dimension (H, N, N, L)
+        u_r_repeat = u_r.unsqueeze(dim=2).repeat(1, 1, n, 1)
+        messages = [
+            l(f_1 * f_2) for f_1, f_2, l
+            in zip(u_r_repeat, p_r, self.message)
+        ]
+
+        aggreagted_messages = self.aggregate(F.relu(
+            torch.cat([
+                (w * m).sum(dim=0) for w, m
+                in zip(weights, messages)
+            ], dim=-1)
+        ))
+        x = self.norm(x + aggreagted_messages)
+
+        if self.return_weights:
+            attn = weights
+        else:
+            attn = None
+
+        return x, attn
+class AttentionLayer(Module):
+    def __init__(self,
+        hidden_size: int = 1024,
+        num_heads: int = 8
+    ):
+        super().__init__()
+        if hidden_size % num_heads != 0:
+            raise ValueError(
+                f"The given hidden size {hidden_size} should be divisible by "
+                f"the number of attention heads {num_heads}."
+            )
+        self.sub_hidden_size = int(hidden_size / num_heads)
+        self.hidden_size = hidden_size
+        self.num_heads = num_heads
+
+        self.unary = nn.Linear(hidden_size, hidden_size)
+        self.pairwise = nn.Linear(hidden_size, hidden_size)
+        self.attn = nn.ModuleList([nn.Linear(3 * self.sub_hidden_size, 1) for _ in range(num_heads)])
+
+    def reshape(self, x: Tensor) -> Tensor:
+        new_x_shape = x.size()[:-1] + (
+            self.num_heads,
+            self.sub_hidden_size
+        )
+        x = x.view(*new_x_shape)
+        if len(new_x_shape) == 3:
+            return x.permute(1, 0, 2)
+        elif len(new_x_shape) == 4:
+            return x.permute(2, 0, 1, 3)
+        else:
+            raise ValueError("Incorrect tensor shape")
+
+    def forward(self, x: Tensor, y: Tensor, i: Tensor, j: Tensor):
+
+        u = F.relu(self.unary(x))
+        p = F.relu(self.pairwise(y))
+
+        # Unary features (H, N, L)
+        u_r = self.reshape(u)
+        # Pairwise features (H, N, N, L)
+        p_r = self.reshape(p)
+
+        # Features used to compute attention (H, M, 3L)
+        attn_features = torch.cat([
+            u_r[:, i], u_r[:, j], p_r[:, i, j]
+        ], dim=-1)
+        # Attention weights (H, M)
+        weights = torch.stack([
+            l(f) for f, l
+            in zip(attn_features, self.attn)
+        ]).squeeze(-1)
+
+        return weights
+
 class InteractionHead(Module):
     """Interaction head that constructs and classifies box pairs
 
@@ -543,188 +728,6 @@ class InteractionHead(Module):
 
         return results
 
-class MultiBranchFusion(Module):
-    """
-    Multi-branch fusion module
-
-    Parameters:
-    -----------
-    appearance_size: int
-        Size of the appearance features
-    spatial_size: int
-        Size of the spatial features
-    representation_size: int
-        Size of the intermediate representations
-    cardinality: int
-        The number of homogeneous branches
-    """
-    def __init__(self,
-        appearance_size: int, spatial_size: int,
-        representation_size: int, cardinality: int
-    ) -> None:
-        super().__init__()
-        self.cardinality = cardinality
-
-        sub_repr_size = int(representation_size / cardinality)
-        assert sub_repr_size * cardinality == representation_size, \
-            "The given representation size should be divisible by cardinality"
-
-        self.fc_1 = nn.ModuleList([
-            nn.Linear(appearance_size, sub_repr_size)
-            for _ in range(cardinality)
-        ])
-        self.fc_2 = nn.ModuleList([
-            nn.Linear(spatial_size, sub_repr_size)
-            for _ in range(cardinality)
-        ])
-        self.fc_3 = nn.ModuleList([
-            nn.Linear(sub_repr_size, representation_size)
-            for _ in range(cardinality)
-        ])
-    def forward(self, appearance: Tensor, spatial: Tensor) -> Tensor:
-        return F.relu(torch.stack([
-            fc_3(F.relu(fc_1(appearance) * fc_2(spatial)))
-            for fc_1, fc_2, fc_3
-            in zip(self.fc_1, self.fc_2, self.fc_3)
-        ]).sum(dim=0))
-
-class MatchingLayer(Module):
-    def __init__(self,
-        hidden_size: int = 1024,
-        num_heads: int = 8,
-        return_weights: bool = False,
-    ):
-        super().__init__()
-        if hidden_size % num_heads != 0:
-            raise ValueError(
-                f"The given hidden size {hidden_size} should be divisible by "
-                f"the number of attention heads {num_heads}."
-            )
-        self.sub_hidden_size = int(hidden_size / num_heads)
-        self.hidden_size = hidden_size
-        self.num_heads = num_heads
-        self.return_weights = return_weights
-
-        self.unary = nn.Linear(hidden_size, hidden_size)
-        self.pairwise = nn.Linear(hidden_size, hidden_size)
-        self.attn = nn.ModuleList([nn.Linear(3 * self.sub_hidden_size, 1) for _ in range(num_heads)])
-        self.message = nn.ModuleList([nn.Linear(self.sub_hidden_size, self.sub_hidden_size) for _ in range(num_heads)])
-        self.aggregate = nn.Linear(hidden_size, hidden_size)
-        self.norm = nn.LayerNorm(hidden_size)
-
-    def reshape(self, x: Tensor) -> Tensor:
-        new_x_shape = x.size()[:-1] + (
-            self.num_heads,
-            self.sub_hidden_size
-        )
-        x = x.view(*new_x_shape)
-        if len(new_x_shape) == 3:
-            return x.permute(1, 0, 2)
-        elif len(new_x_shape) == 4:
-            return x.permute(2, 0, 1, 3)
-        else:
-            raise ValueError("Incorrect tensor shape")
-
-    def forward(self, x: Tensor, y: Tensor):
-        device = x.device
-        n = len(x)
-
-        u = F.relu(self.unary(x))
-        p = F.relu(self.pairwise(y))
-
-        # Unary features (H, N, L)
-        u_r = self.reshape(u)
-        # Pairwise features (H, N, N, L)
-        p_r = self.reshape(p)
-
-        i, j = torch.meshgrid(
-            torch.arange(n, device=device),
-            torch.arange(n, device=device)
-        )
-
-        # Features used to compute attention (H, N, N, 3L)
-        attn_features = torch.cat([
-            u_r[:, i], u_r[:, j], p_r
-        ], dim=-1)
-        # Attention weights (H,) (N, N, 1)
-        weights = [
-            F.softmax(l(f), dim=0) for f, l
-            in zip(attn_features, self.attn)
-        ]
-        # Repeated unary feaures along the third dimension (H, N, N, L)
-        u_r_repeat = u_r.unsqueeze(dim=2).repeat(1, 1, n, 1)
-        messages = [
-            l(f_1 * f_2) for f_1, f_2, l
-            in zip(u_r_repeat, p_r, self.message)
-        ]
-
-        aggreagted_messages = self.aggregate(F.relu(
-            torch.cat([
-                (w * m).sum(dim=0) for w, m
-                in zip(weights, messages)
-            ], dim=-1)
-        ))
-        x = self.norm(x + aggreagted_messages)
-
-        if self.return_weights:
-            attn = weights
-        else:
-            attn = None
-
-        return x, attn
-class AttentionLayer(Module):
-    def __init__(self,
-        hidden_size: int = 1024,
-        num_heads: int = 8
-    ):
-        super().__init__()
-        if hidden_size % num_heads != 0:
-            raise ValueError(
-                f"The given hidden size {hidden_size} should be divisible by "
-                f"the number of attention heads {num_heads}."
-            )
-        self.sub_hidden_size = int(hidden_size / num_heads)
-        self.hidden_size = hidden_size
-        self.num_heads = num_heads
-
-        self.unary = nn.Linear(hidden_size, hidden_size)
-        self.pairwise = nn.Linear(hidden_size, hidden_size)
-        self.attn = nn.ModuleList([nn.Linear(3 * self.sub_hidden_size, 1) for _ in range(num_heads)])
-
-    def reshape(self, x: Tensor) -> Tensor:
-        new_x_shape = x.size()[:-1] + (
-            self.num_heads,
-            self.sub_hidden_size
-        )
-        x = x.view(*new_x_shape)
-        if len(new_x_shape) == 3:
-            return x.permute(1, 0, 2)
-        elif len(new_x_shape) == 4:
-            return x.permute(2, 0, 1, 3)
-        else:
-            raise ValueError("Incorrect tensor shape")
-
-    def forward(self, x: Tensor, y: Tensor, i: Tensor, j: Tensor):
-
-        u = F.relu(self.unary(x))
-        p = F.relu(self.pairwise(y))
-
-        # Unary features (H, N, L)
-        u_r = self.reshape(u)
-        # Pairwise features (H, N, N, L)
-        p_r = self.reshape(p)
-
-        # Features used to compute attention (H, M, 3L)
-        attn_features = torch.cat([
-            u_r[:, i], u_r[:, j], p_r[:, i, j]
-        ], dim=-1)
-        # Attention weights (H, M)
-        weights = torch.stack([
-            l(f) for f, l
-            in zip(attn_features, self.attn)
-        ]).squeeze(-1)
-
-        return weights
 class GraphHead(Module):
     """
     Graphical model head
