904447e46f0843faa71527d69354dfc0b9a24c4a
fred@pegasus <frederic.zhang@anu.edu.au>
2021-10-20 21:25:55 +1100

    Implement build detector method
    Signed-off-by: fred@pegasus <frederic.zhang@anu.edu.au>

--- detector.py
+++ detector.py
@@ -7,12 +7,11 @@ The Australian National University
 Australian Centre for Robotic Vision
 """
 
+from math import gamma
+import os
 import torch
 import torch.distributed as dist
 
-import sys
-sys.path.append('detr')
-from util.misc import nested_tensor_from_tensor_list
 
 from torch import nn, Tensor
 from typing import Optional, List
@@ -20,6 +19,12 @@ from torchvision.ops.boxes import batched_nms, box_iou
 
 from ops import binary_focal_loss
 from interaction_head import InteractionHead
+
+import sys
+sys.path.append('detr')
+from models import build_model
+from util.misc import nested_tensor_from_tensor_list
+
 class GenericHOIDetector(nn.Module):
     """A generic architecture for HOI detector
 
@@ -248,71 +253,22 @@ class GenericHOIDetector(nn.Module):
         detections = self.postprocessing(boxes, bh, bo, logits, prior, objects, attn_maps)
         return detections
 
-# class SpatiallyConditionedGraph(GenericHOINetwork):
-#     def __init__(self,
-#         object_to_action: List[list],
-#         human_idx: int,
-#         # Backbone parameters
-#         backbone_name: str = "resnet50",
-#         pretrained: bool = True,
-#         # Pooler parameters
-#         output_size: int = 7,
-#         sampling_ratio: int = 2,
-#         # Box pair head parameters
-#         node_encoding_size: int = 1024,
-#         representation_size: int = 1024,
-#         num_classes: int = 117,
-#         box_score_thresh: float = 0.2,
-#         fg_iou_thresh: float = 0.5,
-#         num_iterations: int = 2,
-#         distributed: bool = False,
-#         # Transformation parameters
-#         min_size: int = 800, max_size: int = 1333,
-#         image_mean: Optional[List[float]] = None,
-#         image_std: Optional[List[float]] = None,
-#         postprocess: bool = True,
-#         # Preprocessing parameters
-#         box_nms_thresh: float = 0.5,
-#         max_human: int = 15,
-#         max_object: int = 15
-#     ) -> None:
-
-#         detector = models.fasterrcnn_resnet_fpn(backbone_name,
-#             pretrained=pretrained)
-#         backbone = detector.backbone
-
-#         box_roi_pool = MultiScaleRoIAlign(
-#             featmap_names=['0', '1', '2', '3'],
-#             output_size=output_size,
-#             sampling_ratio=sampling_ratio
-#         )
-
-#         box_pair_predictor = nn.Linear(representation_size * 2, num_classes)
-
-#         interaction_head = InteractionHead(
-#             box_roi_pool=box_roi_pool,
-#             box_pair_predictor=box_pair_predictor,
-#             out_channels=backbone.out_channels,
-#             roi_pool_size=output_size,
-#             node_encoding_size=node_encoding_size,
-#             representation_size=representation_size,
-#             human_idx=human_idx,
-#             num_classes=num_classes,
-#             object_class_to_target_class=object_to_action,
-#             num_iter=num_iterations,
-#             box_nms_thresh=box_nms_thresh,
-#             box_score_thresh=box_score_thresh,
-#             fg_iou_thresh=fg_iou_thresh,
-#             max_human=max_human,
-#             max_object=max_object,
-#             distributed=distributed
-#         )
-
-#         if image_mean is None:
-#             image_mean = [0.485, 0.456, 0.406]
-#         if image_std is None:
-#             image_std = [0.229, 0.224, 0.225]
-#         transform = HOINetworkTransform(min_size, max_size,
-#             image_mean, image_std)
-
-#         super().__init__(backbone, interaction_head, transform, postprocess)
+def build_detector(args, class_corr):
+    detr, criterion, postprocessors = build_model(args)
+    if os.path.exists(args.pretrained):
+        print(f"Load pre-trained model from {args.pretrained}")
+        detr.load_state_dict(torch.load(args.pretrained)['model_state_dict'])
+    predictor = torch.nn.Linear(args.hidden_dim * 2, args.num_classes)
+    interaction_head = InteractionHead(predictor, args.hidden_dim, class_corr)
+    detector = GenericHOIDetector(
+        detr, criterion, postprocessors, interaction_head,
+        box_score_thresh=args.box_score_thresh,
+        fg_iou_thresh=args.fg_iou_thresh,
+        human_idx=args.human_idx, num_classes=args.num_classes,
+        alpha=args.alpha, gamma=args.gamma,
+        min_h_instances=args.min_h,
+        max_h_instances=args.max_h,
+        min_o_instances=args.min_o,
+        max_o_instances=args.max_o
+    )
+    return detector
