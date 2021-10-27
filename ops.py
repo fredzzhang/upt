861041e32c10a8c671d40c1b9e77420cc135ae73
fred@pegasus <frederic.zhang@anu.edu.au>
2021-10-27 19:41:46 +1100

    Add conversion functions for box formats
    Signed-off-by: fred@pegasus <frederic.zhang@anu.edu.au>

--- ops.py
+++ ops.py
@@ -183,6 +183,17 @@ class SetCriterion(nn.Module):
             interaction_loss=self.interaction_loss(bx_h, bx_o, indices, prior, logits, targets)
         )
 
+def box_cxcywh_to_xyxy(x):
+    x_c, y_c, w, h = x.unbind(-1)
+    b = [(x_c - 0.5 * w), (y_c - 0.5 * h),
+         (x_c + 0.5 * w), (y_c + 0.5 * h)]
+    return torch.stack(b, dim=-1)
+
+def box_xyxy_to_cxcywh(x):
+    x0, y0, x1, y1 = x.unbind(-1)
+    b = [(x0 + x1) / 2, (y0 + y1) / 2,
+         (x1 - x0), (y1 - y0)]
+    return torch.stack(b, dim=-1)
 
 def compute_spatial_encodings(
     boxes_1: List[Tensor], boxes_2: List[Tensor],
