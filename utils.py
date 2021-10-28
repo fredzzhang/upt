421aad31eeb5c03937a71e58d93f430022bdb001
fred@pegasus <frederic.zhang@anu.edu.au>
2021-10-28 19:35:50 +1100

    Apply colour jittering and random cropping in training
    Signed-off-by: fred@pegasus <frederic.zhang@anu.edu.au>

--- utils.py
+++ utils.py
@@ -73,10 +73,18 @@ class DataFactory(Dataset):
             T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ])
         scales = [480, 512, 544, 576, 608, 640, 672, 704, 736, 768, 800]
-        if partition.startswith('train'):
+        if self.training:
             self.transforms = T.Compose([
                 T.RandomHorizontalFlip(),
-                T.RandomResize(scales, max_size=1333),
+                T.ColorJitter(.4, .4, .4),
+                T.RandomSelect(
+                    T.RandomResize(scales, max_size=1333),
+                    T.Compose([
+                        T.RandomResize([400, 500, 600]),
+                        T.RandomSizeCrop(384, 600),
+                        T.RandomResize(scales, max_size=1333),
+                    ])
+                ),
                 normalize,
             ])
         else:
