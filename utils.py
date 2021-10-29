e2c7f649e3176fb32326650731d42684669bf1a1
fred@woodside <frederic.zhang@anu.edu.au>
2021-10-29 18:07:47 +1100

    Remove random crop
    Signed-off-by: fred@woodside <frederic.zhang@anu.edu.au>

--- utils.py
+++ utils.py
@@ -77,14 +77,7 @@ class DataFactory(Dataset):
             self.transforms = T.Compose([
                 T.RandomHorizontalFlip(),
                 T.ColorJitter(.4, .4, .4),
-                T.RandomSelect(
-                    T.RandomResize(scales, max_size=1333),
-                    T.Compose([
-                        T.RandomResize([400, 500, 600]),
-                        T.RandomSizeCrop(384, 600),
-                        T.RandomResize(scales, max_size=1333),
-                    ])
-                ),
+                T.RandomResize(scales, max_size=1333),
                 normalize,
             ])
         else:
